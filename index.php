ba158ec36fd189a5f010e612d33dfe95b5e77166
Italo Luiz <italo.luis.s@gmail.com>
2021-01-17 18:51:12 -0300

    melhorando as rotas da aplicação

--- index.php
+++ index.php
@@ -14,15 +14,29 @@ require 'src/controllers/locacao.php';
 
 $app = new \Slim\App([['settings' => $config]]);
 
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
+
 
 $app->get('/veiculos', VeiculoController::class . ':listar');
+$app->get('/veiculo/{id}', VeiculoController::class . ':listarPorId');
 $app->get('/veiculosDisponiveis', VeiculoController::class . ':listarDisponiveis');
 $app->post('/veiculo', VeiculoController::class . ':insert');
+$app->get('/veiculosLocados', VeiculoController::class . ':listarLocados');
 $app->put('/veiculo/{id}', VeiculoController::class . ':update');
 $app->delete('/veiculo/{id}', VeiculoController::class . ':delete');
 
 
-$app->get('/veiculosLocados', LocacaoController::class . ':listar');
+$app->get('/locacoes', LocacaoController::class . ':listar');
 $app->post('/locarVeiculo', LocacaoController::class . ':locar');
 $app->put('/devolverVeiculo/{id}', LocacaoController::class . ':devolver');
 
